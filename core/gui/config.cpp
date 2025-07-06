b07c0c0f10770f9b55ee4a35be6d315bd99fdfcf
RomeroPablo <romeropablojr03@gmail.com>
2025-07-06 16:26:41 -0500

    next-gen-gui

--- core/gui/config.cpp
+++ core/gui/config.cpp
@@ -1,6 +1,6 @@
-﻿#include "../settings/functions.h"
+#include "../settings/functions.h"
 
-bool load_button(std::string_view label, bool* callback) {
+bool active_button(std::string_view label, int selection_id, int& selection_variable) {
     ImGuiWindow* window = GetCurrentWindow();
 
     ImGuiContext& g = *GImGui;
@@ -13,58 +13,32 @@ bool load_button(std::string_view label, bool* callback) {
     ItemSize(rect, 0);
     if (!ItemAdd(rect, id)) return false;
 
-    bool hovered = IsItemHovered();
-    bool clicked = hovered && g.IO.MouseClicked[0];
+    const bool selected = selection_id == selection_variable;
+    bool hovered, held, pressed = ButtonBehavior(rect, id, &hovered, &held);
+    if (pressed) selection_variable = selection_id;
 
-    if (clicked)
-        *callback = !*callback;
-    
     float* state = gui->anim_container(&state, id);
-    *state = ImClamp(*state + (gui->fixed_speed(8.f) * (!*callback ? 1.f : -1.f)), 0.f, 1.f);
+    *state = ImClamp(*state + (gui->fixed_speed(8.f) * (!selected ? 1.f : -1.f)), 0.f, 1.f);
 
     draw->add_rect_filled(window->DrawList, rect.Min, rect.Max, gui->get_clr(clr->c_lua.stroke), SCALE(2.f));
-    draw->render_text(window->DrawList, set->c_font.inter_medium[1], rect.Min, rect.Max, gui->get_clr(clr->c_text.text), "UNLOAD", NULL, NULL, ImVec2(0.5f, 0.5f));
+    draw->render_text(window->DrawList, set->c_font.inter_medium[1], rect.Min, rect.Max, gui->get_clr(clr->c_text.text), "LOADED", NULL, NULL, ImVec2(0.5f, 0.5f));
 
     draw->add_rect_filled(window->DrawList, rect.Min, rect.Max, gui->get_clr(clr->c_other_clr.accent_clr, *state), SCALE(2.f));
     draw->render_text(window->DrawList, set->c_font.inter_medium[1], rect.Min, rect.Max, gui->get_clr(clr->c_text.text_active, *state), "LOAD", NULL, NULL, ImVec2(0.5f, 0.5f));
-    
-    return clicked;
-}
 
-bool c_widget::lua_tool_button(std::string_view icon, std::string_view aid)
-{
-    ImGuiWindow* window = GetCurrentWindow();
-
-    ImGuiContext& g = *GImGui;
-    const ImGuiStyle& style = g.Style;
-    const ImGuiID id = window->GetID((std::stringstream{} << icon << aid).str().c_str());
-
-    const ImVec2 pos = window->DC.CursorPos;
-    ImRect rect(pos, pos + SCALE(15, 15));
-
-    ItemSize(rect, 0);
-    if (!ItemAdd(rect, id)) return false;
-
-    bool hovered, held;
-    bool pressed = ButtonBehavior(rect, id, &hovered, &held);
-    
-    ImVec4* state = gui->anim_container(&state, id);
-    *state = ImLerp(*state, hovered ? clr->c_text.text_active : clr->c_text.text_hov, gui->fixed_speed(12.f));
-    
-    draw->render_text(window->DrawList, set->c_font.icon[3], rect.Min, rect.Max, gui->get_clr(*state), icon.data(), NULL, NULL, ImVec2(0.5f, 0.5f));
-    
     return pressed;
 }
 
-bool c_widget::lua_selectable(lua_data* data, int lua_id)
+bool c_widget::config_selectable(config_data* data, int selection_id, int& selection_variable)
 {
-    
     ImGuiWindow* window = GetCurrentWindow();
     if (window->SkipItems) return false;
 
     ImGuiContext& g = *GImGui;
     const ImGuiStyle& style = g.Style;
-    const ImGuiID id = window->GetID((std::stringstream{} << data->name.data() << lua_id).str().c_str());
+    const ImGuiID id = window->GetID((std::stringstream{} << data->name << selection_id).str().c_str());
+
+    const bool selected = selection_id == selection_variable;
 
     const ImVec2 pos = window->DC.CursorPos;
     const float width = GetContentRegionAvail().x;
@@ -72,7 +46,7 @@ bool c_widget::lua_selectable(lua_data* data, int lua_id)
 
     ItemSize(rect, 0);
     if (!ItemAdd(rect, id)) return false;
-    
+
     draw->add_rect_filled(window->DrawList, rect.Min, rect.Max, gui->get_clr(clr->c_lua.background), SCALE(set->c_child.rounding));
     draw->add_rect(window->DrawList, rect.Min, rect.Max, gui->get_clr(clr->c_lua.stroke), SCALE(set->c_child.rounding), 0, SCALE(1.f));
     draw->render_text(window->DrawList, set->c_font.inter_medium[1], rect.Min + SCALE(10, 11), rect.Max, gui->get_clr(clr->c_text.text_active), data->name.data());
@@ -82,16 +56,13 @@ bool c_widget::lua_selectable(lua_data* data, int lua_id)
     const ImVec2 stored_cursor_pos = GetCursorScreenPos();
 
     SetCursorScreenPos(ImVec2(rect.Max.x - SCALE(110), rect.Min.y + SCALE(10)));
-    load_button((std::stringstream{} << data->name << id).str(), &data->loaded);
+    active_button((std::stringstream{} << data->name << id).str(), selection_id, selection_variable);
 
     SetCursorScreenPos(ImVec2(rect.Max.x - SCALE(146), rect.Min.y + SCALE(23)));
-    bool pressed = lua_tool_button("B", std::to_string(id));
-
-    SetCursorScreenPos(ImVec2(rect.Max.x - SCALE(180), rect.Min.y + SCALE(23)));
     if (lua_tool_button("C", std::to_string(id)))
-        var->c_lua.data.erase(var->c_lua.data.begin() + lua_id);
-    
+        var->c_config.data.erase(var->c_config.data.begin() + selection_id);
+
     SetCursorScreenPos(stored_cursor_pos);
-    
-    return pressed;
+
+    return true;
 }
