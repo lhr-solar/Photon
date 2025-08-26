a982a1f480b46677c8801bf8b2e63f9bbfae1a39
RomeroPablo <romeropablojr03@gmail.com>
2025-08-26 14:01:30 -0500

    clean

--- core/config.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-//#define PORT "/dev/ttyUSB0"
-#define PORT "/dev/pts/3"
-
-#define IP "3.141.38.115"
-#define PORT "5000"
-
-#define VERSION "1.0.2"
--- vert.sh
+++
@@ -1,5 +0,0 @@
-glslangValidator -V shaders/glsl/imgui/scene.frag -o shaders/glsl/imgui/scene.frag.spv
-glslangValidator -V shaders/glsl/imgui/scene.vert -o shaders/glsl/imgui/scene.vert.spv
-
-glslangValidator -V shaders/glsl/imgui/ui.frag -o shaders/glsl/imgui/ui.frag.spv
-glslangValidator -V shaders/glsl/imgui/ui.vert -o shaders/glsl/imgui/ui.vert.spv
