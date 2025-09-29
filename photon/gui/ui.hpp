f5c2454edd49d95bd9446f6fab70716081e883ee
Pablo Romero <romeropablojr03@gmail.com>
2025-09-29 07:43:57 -0500

    theme

--- photon/gui/ui.hpp
+++ photon/gui/ui.hpp
@@ -49,6 +49,7 @@ struct UI{
     ImTextureID videoTexture = static_cast<ImTextureID>(0);
     ImVec2 videoTextureSize = ImVec2(0.0f, 0.0f);
 
+    void setStyle();
     void build();
     void fpsWindow();
     void customShaderWindow();
