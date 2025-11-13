3568fd4521358546fec293e1972d58578a8c8155
spvio <speddada22@gmail.com>
2025-11-13 00:31:27 -0600

    glsl logic in comment form

--- photon/parse/parse.cpp
+++ photon/parse/parse.cpp
@@ -1,2 +1,40 @@
 /*[λ] the photon parsing interface*/
 #include "parse.hpp"
+/*[λ] the photon parsing interface*/
+#include "parse.hpp"
+
+
+// layout(local_size_x = 4) in; // 4 threads, one per row
+
+// // Input 
+// layout(binding = 0) buffer InputMatrix {
+//     float A[16];
+// };
+
+// layout(binding = 1) buffer OutputMatrix {
+//     float L[16];
+// };
+
+// void main() {
+//     uint row = gl_GlobalInvocationID.x; // Thread-per-row!!
+
+
+//     const uint N = 4;
+
+//     for (uint col = 0; col < N; col++) {
+
+//         if (row < col) {
+
+//         }
+
+//         if (row == col) {
+//             // Diagonal shii element
+//             L[row * N + col] = sqrt(A[row * N + col] - sum);
+//         }
+//         else {
+
+//             L[row * N + col] = (A[row * N + col] - sum) / L[col * N + col];
+//         }
+//         barrier(); // Synchronize? threads in this workgroup
+//     }
+// }
