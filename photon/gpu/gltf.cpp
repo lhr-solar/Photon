e6a6863486345ae7ac23ea13709e430d8d8e6090
pablo romero <romeropablojr03@gmail.com>
2026-03-13 03:15:36 -0500

    ?

--- photon/gpu/gltf.cpp
+++ photon/gpu/gltf.cpp
@@ -1099,7 +1099,7 @@ void GltfModel::buildGltfPipeline(){
         .binding = 0,
         .descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
         .descriptorCount = 1,
-        .stageFlags = VK_SHADER_STAGE_VERTEX_BIT,
+        .stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
     };
     VkDescriptorSetLayoutCreateInfo frameDslCI = {
         .sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO,
