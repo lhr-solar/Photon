b37f7b7fd6b64aac713139de7081eb0ad37a1ebf
romeroPablo <romeropablojr03@gmail.com>
2026-07-04 18:16:38 -0500

    updater launch fixed

--- photon/gui/updater.cpp
+++ photon/gui/updater.cpp
@@ -1,123 +1,144 @@
 #include "updater.hpp"
 
-#include <windows.h>
+#include <shellapi.h>
 #include <urlmon.h>
+#include <windows.h>
 
-#include <iostream>
 #include <thread>
 
 struct DownloadProgress : IBindStatusCallback {
-    std::atomic<int>& percent;
-    explicit DownloadProgress(std::atomic<int>& p) : percent(p) {}
+  std::atomic<int>& percent;
+  explicit DownloadProgress(std::atomic<int>& p) : percent(p) {}
 
-    ULONG STDMETHODCALLTYPE AddRef() override { return 1; }
-    ULONG STDMETHODCALLTYPE Release() override { return 1; }
+  ULONG STDMETHODCALLTYPE AddRef() override { return 1; }
+  ULONG STDMETHODCALLTYPE Release() override { return 1; }
 
-    HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void** ppv) override {
-        if (!ppv) return E_POINTER;
-        *ppv = (riid == IID_IUnknown || riid == IID_IBindStatusCallback)
-            ? static_cast<IBindStatusCallback*>(this)
-            : nullptr;
-        return *ppv ? S_OK : E_NOINTERFACE;
+  HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void** ppv) override {
+    if (!ppv) return E_POINTER;
+    *ppv = (riid == IID_IUnknown || riid == IID_IBindStatusCallback)
+               ? static_cast<IBindStatusCallback*>(this)
+               : nullptr;
+    return *ppv ? S_OK : E_NOINTERFACE;
+  }
+
+  HRESULT STDMETHODCALLTYPE OnProgress(ULONG p, ULONG m, ULONG s, LPCWSTR t) override {
+    unsigned long long cur = p, total = m;
+
+    if (s == BINDSTATUS_64BIT_PROGRESS && t)
+      if (swscanf_s(t, L"%llu,%llu", &cur, &total) != 2) return S_OK;
+
+    if ((s == BINDSTATUS_DOWNLOADINGDATA || s == BINDSTATUS_ENDDOWNLOADDATA ||
+         s == BINDSTATUS_64BIT_PROGRESS) &&
+        total) {
+      percent = int(double(cur) * 100.0 / double(total));
     }
 
-    HRESULT STDMETHODCALLTYPE OnProgress(ULONG p, ULONG m, ULONG s, LPCWSTR t) override {
-        unsigned long long cur = p, total = m;
+    return S_OK;
+  }
 
-        if (s == BINDSTATUS_64BIT_PROGRESS && t)
-            if (swscanf_s(t, L"%llu,%llu", &cur, &total) != 2) return S_OK;
-
-        if ((s == BINDSTATUS_DOWNLOADINGDATA ||
-             s == BINDSTATUS_ENDDOWNLOADDATA ||
-             s == BINDSTATUS_64BIT_PROGRESS) && total) {
-            percent = int(double(cur) * 100.0 / double(total));
-        }
-
-        return S_OK;
-    }
-
-    HRESULT STDMETHODCALLTYPE OnStartBinding(DWORD, IBinding*) override { return S_OK; }
-    HRESULT STDMETHODCALLTYPE GetPriority(LONG*) override { return E_NOTIMPL; }
-    HRESULT STDMETHODCALLTYPE OnLowResource(DWORD) override { return S_OK; }
-    HRESULT STDMETHODCALLTYPE OnStopBinding(HRESULT, LPCWSTR) override { return S_OK; }
-    HRESULT STDMETHODCALLTYPE GetBindInfo(DWORD*, BINDINFO*) override { return E_NOTIMPL; }
-    HRESULT STDMETHODCALLTYPE OnDataAvailable(DWORD, DWORD, FORMATETC*, STGMEDIUM*) override { return S_OK; }
-    HRESULT STDMETHODCALLTYPE OnObjectAvailable(REFIID, IUnknown*) override { return S_OK; }
+  HRESULT STDMETHODCALLTYPE OnStartBinding(DWORD, IBinding*) override { return S_OK; }
+  HRESULT STDMETHODCALLTYPE GetPriority(LONG*) override { return E_NOTIMPL; }
+  HRESULT STDMETHODCALLTYPE OnLowResource(DWORD) override { return S_OK; }
+  HRESULT STDMETHODCALLTYPE OnStopBinding(HRESULT, LPCWSTR) override { return S_OK; }
+  HRESULT STDMETHODCALLTYPE GetBindInfo(DWORD*, BINDINFO*) override { return E_NOTIMPL; }
+  HRESULT STDMETHODCALLTYPE OnDataAvailable(DWORD, DWORD, FORMATETC*, STGMEDIUM*) override {
+    return S_OK;
+  }
+  HRESULT STDMETHODCALLTYPE OnObjectAvailable(REFIID, IUnknown*) override { return S_OK; }
 };
 
 static std::wstring quote(const std::wstring& s) { return L"\"" + s + L"\""; }
 
+static void debugUpdater(const std::wstring& message) {
+  OutputDebugStringW((L"Photon updater: " + message + L"\n").c_str());
+}
+
 void Updater::launchUpdater() {
-    if (running.exchange(true)) return;
-    photonDownloadPercentage.store(-1);
-    installerDownloadPercentage.store(-1);
-    getOurInfo();
-    std::thread(&Updater::beginUpdate, this).detach();
+  if (running.exchange(true)) return;
+  photonDownloadPercentage.store(-1);
+  installerDownloadPercentage.store(-1);
+  getOurInfo();
+  std::thread(&Updater::beginUpdate, this).detach();
 }
 
 std::filesystem::path getPhotonDownloadPath() {
-    char* local = std::getenv("LOCALAPPDATA");
-    std::filesystem::path dir = std::filesystem::path(local) / "Photon";
-    std::filesystem::create_directories(dir);
-    return dir / "photon.exe";
+  char* local = std::getenv("LOCALAPPDATA");
+  std::filesystem::path dir = std::filesystem::path(local) / "Photon";
+  std::filesystem::create_directories(dir);
+  return dir / "photon.exe";
 }
 
 std::filesystem::path getInstallerDownloadPath() {
-    char* local = std::getenv("LOCALAPPDATA");
-    std::filesystem::path dir = std::filesystem::path(local) / "Photon";
-    std::filesystem::create_directories(dir);
-    return dir / "photonUpdater.exe";
+  char* local = std::getenv("LOCALAPPDATA");
+  std::filesystem::path dir = std::filesystem::path(local) / "Photon";
+  std::filesystem::create_directories(dir);
+  return dir / "photonUpdater.exe";
 }
 
 void Updater::getOurInfo() {
-    ourPid = GetCurrentProcessId();
-    wchar_t buffer[MAX_PATH]{};
-    GetModuleFileNameW(nullptr, buffer, MAX_PATH);
-    ourPath = buffer;
-    photonPath = getPhotonDownloadPath();
-    installerPath = getInstallerDownloadPath();
+  ourPid = GetCurrentProcessId();
+  wchar_t buffer[MAX_PATH]{};
+  GetModuleFileNameW(nullptr, buffer, MAX_PATH);
+  ourPath = buffer;
+  photonPath = getPhotonDownloadPath();
+  installerPath = getInstallerDownloadPath();
 }
 
 void Updater::beginUpdate() {
-    downloadInstaller();
-    downloadNewPhoton();
-    launchInstaller();
+  if (!downloadInstaller()) {
+    debugUpdater(L"installer download failed");
     running = false;
+    return;
+  }
+
+  if (!downloadNewPhoton()) {
+    debugUpdater(L"Photon download failed");
+    running = false;
+    return;
+  }
+
+  if (!launchInstaller()) {
+    debugUpdater(L"installer launch failed");
+  }
+
+  running = false;
 }
 
 bool Updater::downloadInstaller() {
-    DownloadProgress progress(installerDownloadPercentage);
-    std::string path = installerPath.string();
-    HRESULT hr = URLDownloadToFileA( nullptr, installerURL.c_str(), path.c_str(), 0, &progress );
-    if (hr == S_OK) return true;
-    return false;
+  DownloadProgress progress(installerDownloadPercentage);
+  std::string path = installerPath.string();
+  HRESULT hr = URLDownloadToFileA(nullptr, installerURL.c_str(), path.c_str(), 0, &progress);
+  if (hr == S_OK) return true;
+  return false;
 }
 
 bool Updater::downloadNewPhoton() {
-    DownloadProgress progress(photonDownloadPercentage);
-    std::string path = photonPath.string();
-    HRESULT hr = URLDownloadToFileA( nullptr, photonURL.c_str(), path.c_str(), 0, &progress );
-    if (hr == S_OK) return true;
-    return false;
+  DownloadProgress progress(photonDownloadPercentage);
+  std::string path = photonPath.string();
+  HRESULT hr = URLDownloadToFileA(nullptr, photonURL.c_str(), path.c_str(), 0, &progress);
+  if (hr == S_OK) return true;
+  return false;
 }
 
-void Updater::launchInstaller() {
-    STARTUPINFOW si{};
-    PROCESS_INFORMATION pi{};
-    si.cb = sizeof(si);
+bool Updater::launchInstaller() {
+  const std::wstring exe = installerPath.wstring();
+  const std::wstring parameters = std::to_wstring(ourPid) + L" " + quote(ourPath);
+  std::wstring workingDirectory = installerPath.parent_path().wstring();
 
-    std::wstring exe = installerPath.wstring();
+  SHELLEXECUTEINFOW sei{};
+  sei.cbSize = sizeof(sei);
+  sei.fMask = SEE_MASK_NOCLOSEPROCESS;
+  sei.lpVerb = L"runas";
+  sei.lpFile = exe.c_str();
+  sei.lpParameters = parameters.c_str();
+  sei.lpDirectory = workingDirectory.c_str();
+  sei.nShow = SW_SHOWNORMAL;
 
-    std::wstring cmd =
-        quote(exe) + L" " +
-        std::to_wstring(ourPid) + L" " +
-        quote(ourPath);
+  if (!ShellExecuteExW(&sei)) {
+    debugUpdater(L"ShellExecuteExW failed with error " + std::to_wstring(GetLastError()));
+    return false;
+  }
 
-    BOOL ok = CreateProcessW(exe.c_str(), cmd.data(), nullptr, nullptr, FALSE, 0, nullptr,
-        nullptr, &si, &pi );
-
-    if (!ok) return;
-
-    CloseHandle(pi.hThread);
-    CloseHandle(pi.hProcess);
+  if (sei.hProcess) CloseHandle(sei.hProcess);
+  return true;
 }
--- photon/gui/updater.hpp
+++ photon/gui/updater.hpp
@@ -1,31 +1,32 @@
 #pragma once
+#include <windows.h>
+
 #include <atomic>
 #include <filesystem>
 #include <string>
-#include <windows.h>
 
 struct Updater {
-    std::atomic<int> photonDownloadPercentage{-1};
-    std::atomic<int> installerDownloadPercentage{-1};
-    std::atomic<bool> running{false};
+  std::atomic<int> photonDownloadPercentage{-1};
+  std::atomic<int> installerDownloadPercentage{-1};
+  std::atomic<bool> running{false};
 
-    std::filesystem::path installerPath{};
-    std::string installerURL = 
-        "https://github.com/lhr-solar/Photon/releases/download/windowsUpdater/photonUpdater.exe";
+  std::filesystem::path installerPath{};
+  std::string installerURL =
+      "https://github.com/lhr-solar/Photon/releases/download/windowsUpdater/photonUpdater.exe";
 
-    std::filesystem::path photonPath{};
-    std::string photonURL =
-        "https://github.com/lhr-solar/Photon/releases/download/Win_Pre-Release/Photon.exe";
+  std::filesystem::path photonPath{};
+  std::string photonURL =
+      "https://github.com/lhr-solar/Photon/releases/download/Win_Pre-Release/Photon.exe";
 
-    DWORD ourPid{};
-    std::wstring ourPath{};
+  DWORD ourPid{};
+  std::wstring ourPath{};
 
-    std::string version = "00.00.01";
+  std::string version = "00.00.01";
 
-    void getOurInfo();
-    bool downloadInstaller();
-    bool downloadNewPhoton();
-    void launchInstaller();
-    void launchUpdater();
-    void beginUpdate();
+  void getOurInfo();
+  bool downloadInstaller();
+  bool downloadNewPhoton();
+  bool launchInstaller();
+  void launchUpdater();
+  void beginUpdate();
 };
